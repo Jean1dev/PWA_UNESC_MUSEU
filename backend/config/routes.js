5206e64ff3095ff819e1e40741273b3c416e8fa7
jean <jeanlucafp@gmail.com>
2018-05-11 20:08:20 -0300

    add controllers and routes
    ...

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -3,6 +3,16 @@ module.exports = server =>{
     /*****************************
      * Definir aqui as URL`s rest
      */
-    
+    //AGENDAMENTOS
+    server.post('/agendamento', AgendamentoController.post)
+    server.get('/agendamento', AgendamentoController.getAll)
+
+    //ANIMAIS
+    server.post('/animais', AnimaisController.post)
+    server.get('/animais', AnimaisController.getAll)
+
+    //INFO MUSEU
+    server.post('/museu', MuseuController.post)
+    server.get('/museu', MuseuController.getAll)
     
     }
